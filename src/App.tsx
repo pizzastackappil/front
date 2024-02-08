2449bccfaed2537d928ec324868d3d73af2ed590
davidAffman <affmandavid@gmail.com>
2024-02-09 00:49:52 +0200

    Created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-
+import { Header } from "./common/components/header/header.components";
+import { MenuList } from "./modules/menu/components/menu-list/menu-list.components";
+import pizzaMenu from '../src/mocks/pizza.json'
+import { Footer } from "./common/components/footer/footer.components";
 export const  App = () => {
   return (
-    <h1 className="font-bold text-3xl text-red-500 ">🍕 PizzaStack</h1>
+    <div>
+      <Header/>
+      <div className="mx-2 mb-24">
+       <MenuList items={pizzaMenu}/>
+      </div>
+      <Footer/>
+    </div>
   );
 }
 
--- src/common/components/footer/footer.components.tsx
+++ src/common/components/footer/footer.components.tsx
@@ -3,7 +3,7 @@ import { FooterLink } from "../footer_link/footer-link.components";
 
 export const Footer = () => {
   return (
-    <div className="bg-gray-900 pt-12 px-12 pb-8">
+    <div className="bg-gray-900 p-12">
       <div>
         <div className="mb-4">
           <span className="uppercase font-bold text-sm text-zing-500">
